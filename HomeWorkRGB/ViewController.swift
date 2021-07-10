16a4cc189822428bffc811efe745d6e6e3c2a34a
rkateev <60319058+rkateev@users.noreply.github.com>
2021-07-10 19:22:33 +0300

    fix interface

--- HomeWorkRGB/ViewController.swift
+++ HomeWorkRGB/ViewController.swift
@@ -8,10 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var coloredView: UIView!
+    @IBOutlet weak var redLabel: UILabel!
+    @IBOutlet weak var redValue: UILabel!
+    @IBOutlet weak var greenLabel: UILabel!
+    @IBOutlet weak var greenValue: UILabel!
+    @IBOutlet weak var blueLabel: UILabel!
+    @IBOutlet weak var blueValue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redValue.text = "0.00"
+        greenValue.text = "0.00"
+        blueValue.text = "0.00"
+        
     }
 
 
